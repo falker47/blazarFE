bfb43327c230afef2a274c9a1d494c96560e8d15
falker <maurizio.falconi47@gmail.com>
2023-04-01 13:36:16 +0200

    aggiunto esercizio HackerRank difficoltà media

--- hacker_rank_exercises.js
+++ hacker_rank_exercises.js
@@ -3,26 +3,25 @@
 //possibile efficientarlo ulteriormente con ciò che abbiamo già visto
 
 function reverseArray(a) {
-    let n=0;   
-       for (const element of a){
-           n++
+    let n = 0;
+    for (const element of a) {
+        n++
     }
-       for (let i=0; i<n-1; i++){
-           a.unshift(a[(2*i)+1
+    for (let i = 0; i < n - 1; i++) {
+        a.unshift(a[(2 * i) + 1
         ]);
     }
-       for(let i=0; i<n-1; i++)
-       {
-           a.pop()
+    for (let i = 0; i < n - 1; i++) {
+        a.pop()
     }
-       return a;
+    return a;
 }
 
 
 // Esercizio 2
 function simpleArraySum(ar) {
     let sum = 0;
-    for (const element of ar){
+    for (const element of ar) {
         sum += element;
     }
     return sum;
@@ -31,31 +30,40 @@ function simpleArraySum(ar) {
 
 // Esercizio 3
 function staircase(n) {
-    for (let i=1; i<=n; i++)
-    {
+    for (let i = 1; i <= n; i++) {
         let string = "";
-        for(let spaces=1; spaces<=n-i; spaces++) //spaces rappresenta il n di hashtag da stampare
+        for (let spaces = 1; spaces <= n - i; spaces++) //spaces rappresenta il n di hashtag da stampare
         {
             string += " "
         }
-        for(let hashtags=1; hashtags<=i; hashtags++) //hashtags rappresenta il n di hashtag da stampare
+        for (let hashtags = 1; hashtags <= i; hashtags++) //hashtags rappresenta il n di hashtag da stampare
         {
             string += "#";
         }
-        
+
         console.log(string);
     }
 }
 
-// Esercizio 4 (giusto al primo tentativo, ci sto prendendo gusto ;))
+// alternativa esercizio 3
+function staircase(n) {
+    let string = "";
+    let space = " ";
+    let hashtag = "#";
+    for (let i = 1; i <= n; i++) {
+        string += space.repeat(n - i);
+        string += hashtag.repeat(i);
+        string += "\n";
+    }
+    console.log(string);
+}
+
+// Esercizio 4
 function divisibleSumPairs(n, k, ar) {
     let counter_of_magic_number = 0;
-    for(let i=0; i<n; i++)
-    {
-        for(let j=i+1; j<n; j++)
-        {
-            if( (ar[i]+ar[j])%k == 0)
-            {
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if ((ar[i] + ar[j]) % k == 0) {
                 counter_of_magic_number++;
             }
         }
@@ -66,40 +74,46 @@ function divisibleSumPairs(n, k, ar) {
 // Esercizio 5
 function compareTriplets(a, b) {
     let scoreboard = [0, 0];
-    for(let i=0; i<3; i++)
-    {
-        if(a[i] > b[i])
-        {
+    for (let i = 0; i < 3; i++) {
+        if (a[i] > b[i]) {
             scoreboard[0]++;
         }
-        else if(a[i] < b[i])
-        {
+        else if (a[i] < b[i]) {
             scoreboard[1]++;
         }
     }
     return scoreboard;
-    }
+}
 
 // Esercizio 6
 function hourglassSum(arr) {
-    let hourglass_max_sum=-Infinity;
-    let hg_t_sum=0;
-    
-    for(let i=1; i<=4; i++)
-    {
-        for(let j=1; j<=4; j++)
-        {
+    let hourglass_max_sum = -Infinity;
+    let hg_t_sum = 0;
+
+    for (let i = 1; i <= 4; i++) {
+        for (let j = 1; j <= 4; j++) {
             {
                 hg_t_sum = 0;                                        // l'ho scritto così anzichè mettere altri cicli "for" perchè mi sembrava
-                hg_t_sum += arr[i-1][j-1]+arr[i-1][j]+arr[i-1][j+1]; // più leggibile e carino (in teoria non ha nemmeno perso efficienza, giusto?)
+                hg_t_sum += arr[i - 1][j - 1] + arr[i - 1][j] + arr[i - 1][j + 1]; // più leggibile e carino (in teoria non ha nemmeno perso efficienza, giusto?)
                 hg_t_sum += arr[i][j];                               // se avessi scritto la somma con tutti gli addendi in una botta
-                hg_t_sum += arr[i+1][j-1]+arr[i+1][j]+arr[i+1][j+1]; // sola sarebbe stato più efficiente?
-                if(hg_t_sum > hourglass_max_sum)                      
-                {
-                hourglass_max_sum = hg_t_sum;
+                hg_t_sum += arr[i + 1][j - 1] + arr[i + 1][j] + arr[i + 1][j + 1]; // sola sarebbe stato più efficiente?
+                if (hg_t_sum > hourglass_max_sum) {
+                    hourglass_max_sum = hg_t_sum;
                 }
             }
         }
     }
     return hourglass_max_sum;
+}
+
+// Esercizio difficoltà Media: Extra long factorial
+
+function extraLongFactorials(n) {
+    let factorial = 1n;
+    let bigN = BigInt(n)
+    for(let i=1n; i<=bigN; i++)
+    {
+        factorial *= i;
+    }
+    console.log(String(factorial)) ;
 }
