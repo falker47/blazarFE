a30a02a5359b16185d2cef3b130714323b4c171a
falker <maurizio.falconi47@gmail.com>
2023-04-17 16:45:08 +0200

    new social (l'altro era buggato)

---
+++ instantanea/src/app/social-logo/social-logo.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SocialLogoComponent } from './social-logo.component';
+
+describe('SocialLogoComponent', () => {
+  let component: SocialLogoComponent;
+  let fixture: ComponentFixture<SocialLogoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SocialLogoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SocialLogoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
